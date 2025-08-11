827c5290d7b5acd76aac31e8da477423bae65ec7
Abraham Li <abraham.li@s.elimmtalbert.school.nz>
2025-08-11 15:17:09 +1200

    adding back and forward buttons to the video

--- 2025/javascript/script.js
+++ 2025/javascript/script.js
@@ -1,5 +1,5 @@
 var video = document.getElementById("video");
-
+//play and pause functions
 function playPause(){
     if (video.paused){
         video.play();
@@ -7,5 +7,13 @@ function playPause(){
     else{
         video.pause();
     }
+}
 
+//back and forward 10 seconds but not going below zero seconds which is the start
+function back10(){
+    video.currentTime = Math.max(0, video.currentTime - 10);
+}
+
+function forward10(){
+    video.currentTime = Math.max(0, video.currentTime + 10);
 }
